f50c89126f652899a8ec7839dbcc87c2193bc35d
Your Name <you@example.com>
2017-02-20 11:22:30 +0100

    nodeJS Express API maken

---
+++ nodeJS/H5_Webapps met Express/API/server.js
@@ -0,0 +1,26 @@
+/**
+ * Created by switch87 on 20/02/17.
+ */
+var express = require('express');
+var app = express();
+
+// 2. Haal de gegevens binnen de app
+var auteurs = require('./auteurs.json');
+var boeken = require('./boeken.json');
+
+app.get('/', function (req, res) {
+    var msg = '<h1>Express API</h1>';
+    msg += '<p>Gebruik \\api\\auteurs voor een lijst met auteurs.</p>';
+    msg += '<p>Gebruik \\api\\boeken voor een lijst met boeken.</p>';
+    res.send(msg);
+});
+
+app.get('/api/auteurs', function (req, res) {
+    res.json(auteurs);
+});
+app.get('/api/boeken', function (req, res) {
+    res.json(boeken);
+});
+
+app.listen(3000);
+console.log('Express-server gestart op http://localhost:3000');
