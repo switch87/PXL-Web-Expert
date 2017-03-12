5ca7a8ff6758b5c5b4d29ffd60c69d92ede2c24c
switch87 <gert@pellin.be>
2017-03-12 12:53:10 +0100

    AngularJS: controllerAs-sybtaxis

--- AngularJS/js/H03/controller.js
+++ AngularJS/js/H03/controller.js
@@ -5,24 +5,23 @@
 (function (app) {
     app.controller('myController', myControllerFn);
 
-    myControllerFn.$inject = ['$scope', '$location'];
+    function myControllerFn() {
 
-    function myControllerFn($scope, $location) {
-
-        $scope.persons = [
+        var vm = this;
+        vm.persons = [
             {name: 'John', lastname: 'Visser', age: '55', email: 'John@pxl.be'},
             {name: 'Bob', lastname: 'De Bouwer', age: '18', email: 'Bob@pxl.be'},
             {name: 'Nick', lastname: 'Sint Klaas', age: '33', email: 'Nick@pxl.be'}];
 
-        $scope.showDetail = function (person) {
-            $scope.mame = person.name;
-            $scope.lastname = person.lastname;
-            $scope.age = person.age;
-            $scope.email = person.email;
+        vm.showDetail = function (person) {
+            vm.name = person.name;
+            vm.lastname = person.lastname;
+            vm.age = person.age;
+            vm.email = person.email;
         };
 
-        $scope.showDetail($scope.persons[0]);
+        vm.showDetail(vm.persons[0]);
 
-        $scope.reversed = true;
+        vm.reversed = true;
     }
 })(angular.module('myApp')); // angular.module() fungeert als getter
